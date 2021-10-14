6d3491fdf46d09658997594033578e22af9ea285
Misiek M <marcakster@gmail.com>
2021-10-14 22:27:58 +0200

    edit sql file

--- 1634214336.sql
+++ 1634214336.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS `article` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(256) NOT NULL,
-  `description` varchar(50) DEFAULT NULL,
+  `description` text DEFAULT NULL,
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
